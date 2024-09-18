085958d68215efab1e63f54ecf0791dd561b7ae3
Daulet <baimurza.daulet@gmail.com>
2024-09-18 12:14:01 +0500

    Edding css and Go files

--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS posts (
     content TEXT NOT NULL
 );
 
--- Damp posts
+-- Dump posts
 INSERT INTO posts (title, content) VALUES ('Welcome to my blog', 'This is the first post on this blog.');
 INSERT INTO posts (title, content) VALUES ('Go is awesome', 'Let me you why Go is one of the best language.');
 INSERT INTO posts (title, content) VALUES ('SQLite and Go', 'How to use SQLite in Go applications.');
